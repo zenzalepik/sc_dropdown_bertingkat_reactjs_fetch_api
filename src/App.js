bbac137b57953ac789a717185fdb8226821096e3
Zen Zalepik <51511976+zenzalepik@users.noreply.github.com>
2024-12-16 17:05:19 +0700

    Akses di App.js

--- src/App.js
+++ src/App.js
@@ -2,26 +2,32 @@ import logo from './logo.svg';
 import './App.css';
 import {useState} from 'react';
 import Dropdown1Provinces from './Dropdown1Provinces';
-import Dropdown2Regencies from './Dropdown2Regencies';import { LocationProvider } from "./context/LocationContext";
+import Dropdown2Regencies from './Dropdown2Regencies';import { LocationProvider, useLocationContext } from "./context/LocationContext";
 import Dropdown3Districts from './Dropdown3Districts';
 import Dropdown4Villages from './Dropdown4Villages';
 
+function AppContent() {
+      const { setIdProvince, setIdRegency, setIdDistrict, setIdVillage, idProvince, idRegency, idDistrict, idVillage } = useLocationContext();
+      return (
+        <>
+
+        <Dropdown1Provinces />
+        <Dropdown2Regencies/>
+        <Dropdown3Districts/>
+        <Dropdown4Villages/>
+        {idProvince && (<p>setIdProvince {idProvince}<b></b> <br /></p>)}
+        {idRegency && (<p>setIdRegency {idRegency}<b></b> <br /></p>)}
+        {idDistrict && (<p>setIdDistrict {idDistrict}<b></b> <br /></p>)}
+        {idVillage && (<p>setIdVillage {idVillage}<b></b> <br /></p>)}
+        </>
+      )
+}
 
 function App() {
-  const [idProvince, setIdProvince] = useState("");
-
-  // Fungsi untuk menangkap perubahan idProvince
-    const handleProvinceChange = (id) => {
-      console.log("Selected Province ID in App:", id);
-      setIdProvince(id);
-    };
   return (
     <LocationProvider>
     <div className="App">
-    <Dropdown1Provinces />
-    <Dropdown2Regencies/>
-    <Dropdown3Districts/>
-    <Dropdown4Villages/>
+    <AppContent />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header>
